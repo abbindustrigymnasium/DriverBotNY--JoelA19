da1967d991d36d0ac14b8aa921849f270ca804b4
Joel <joel.andersson@abbindustrigymnasium.se>
2020-03-31 13:06:14 +0200

    laddda upp grund

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text">
+        <span>TheSuperEpicLegoRobotOFSuperMegaEpicness</span>
+        
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-content>
+    <div >
+      <v-btn color="success">Sucess</v-btn>
+    <ExampleComp MSG="apa apa apa" />
+      </div>
+          <div>
+            <v-card width="200" height="200" class="test" justify-center> 
+            <v-switch v-model="Direction" class="ma-20" label="direction"></v-switch>
+      </v-card>
+      </div>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import ExampleComp from './components/ExampleComp'
+
+export default {
+  name: 'App',
+  components: {
+    ExampleComp
+  },
+  data () {
+    return {
+      addnew:false,
+      Direction : 1
+      //
+    }
+  }
+}
+</script>
---
+++ src/components/ExampleComp.vue
@@ -0,0 +1,104 @@
+<template>
+ <!-- "HTML"-kod det som syns på sidan. Stor del är gjort med Vuetify med färdiga komponenter. -->
+<v-layout  justify-center>
+   <v-flex xs12>
+  
+  </v-flex>
+      <v-layout row v-if="Show">
+      <v-flex xs6 order-lg2>
+      </v-flex>
+      <v-flex xs6>
+       
+      </v-flex>
+    </v-layout>
+  <v-layout column v-if="!Show">
+      <v-flex xs2 v-for="(item, index) in names" :key="index">
+      </v-flex>
+     <v-flex xs12 sm6 md3>
+        </v-flex>
+    </v-layout>
+    
+  </v-layout>
+
+</template>
+
+<script>
+//Bibliotek som ska användas
+const axios = require('axios');
+
+export default {
+  name: 'ExampleComp',
+   props: {//Data som skickas in i komponenten
+    MSG: {
+      type: String,
+      required: true
+    },
+  },
+  data() {//All data som ska finnas i komponenten
+    return {
+    Showtext:"Visa",
+     name: '',
+     Show:false,
+      names: [
+        'Pelle',
+        'Joel',
+        'Jonas',
+        'Stian',
+        'Lars',
+        'Göran',
+        'Jan',
+      ],
+         colors: [
+        'green',
+        'red',
+        'blue',
+        'amber',
+        'pink',
+        'yellow',
+        'red',
+      ],
+     
+    }
+  },
+  created() { //När komponenten är skapad
+      let date= new Date();
+      console.log("Nu är den skapad "+      date.toLocaleTimeString());
+  },
+  mounted() { //När komponenten är mountad (inladdad)
+        let date= new Date();
+      console.log("Nu är den mountad "+      date.toLocaleTimeString());
+  },
+  computed: {  
+      GetRandomName(){
+          var max=this.names.length;  
+          return this.names[this.Random(0,max)];
+      }
+  },
+  methods: { //Metoder
+    GetAnotherRandomName(){
+          var max=this.names.length;  
+          this.name= this.names[this.Random(0,max)];
+    } ,
+      Random(min,max){
+          var random =Math.floor(Math.random() * (+max - +min)) + +min; 
+          return random;
+    } ,
+    }
+
+}
+</script>
+
+<!-- CSS -->
+<style scoped>
+.big{
+font-size: 25px;
+}
+#Cooltext{
+    color:black;
+    text-decoration: underline; 
+}
+.test{
+  margin: auto;
+}
+
+</style>
