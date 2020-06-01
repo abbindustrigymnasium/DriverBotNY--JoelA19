382113be0d2eefec8af8c8e7c489df2489fbacad
Joel <joel.andersson@abbindustrigymnasium.se>
2020-06-01 10:08:34 +0200

    tog bort onödiga grejer
    bla gamal kod, examplecomp, och kommenterade

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text">
-        <span>TheSuperEpicLegoRobotOfSuperMegaEpicness</span>
+        <!-- Titeln på toppen av webbsidan -->
+        <span>TheSuperEpicLegoRobotOfSuperMegaEpicness</span> 
         
       </v-toolbar-title>
       <v-spacer></v-spacer>
@@ -13,7 +14,9 @@
       
       </div>
     <div >
-      <v-btn color="success" v-on:click="direc='f'; Send(direc + '1024' + ang)">Forward</v-btn>
+      <!-- alla knappar -->
+      <!-- använder send() funktionen för att skicka det meddelande som hänger ihop med knappen -->
+      <v-btn color="success" v-on:click="direc='f'; Send(direc + '1024' + ang)">Forward</v-btn> 
       </div>
       <div >
       <v-btn color="success" v-on:click="direc='b'; Send(direc + '1024' + ang)">Backward</v-btn>
@@ -22,6 +25,8 @@
       <v-btn color="success" v-on:click="direc='b'; Send(direc + '0000' + ang)">Stop</v-btn>
       </div>
           <div>
+            <!-- till skillnad från de andra knapparna ändrar de här bara ang, alltså vilken riktning som bilen svänger åt -->
+            <!-- detta system låter mig styra om bilen kör framåt eller bakåt och åt vilket håll den svänger separat -->
             <v-card width="200" height="200" class="test" justify-center> 
             <v-btn color="success" v-on:click="ang='000'; Send(direc + '1024' + ang)">Left</v-btn>
             <v-btn color="success" v-on:click="ang='180'; Send(direc + '1024' + ang)">Right</v-btn>
@@ -42,7 +47,7 @@ export default {
   },
   name: 'App',
   methods: {
-    //Metoder
+    //Funktionen för att koppla till MQTT
    connect() {
       var mqtt_url = "maqiatto.com";
       var url = "mqtt://" + mqtt_url;
@@ -82,18 +87,16 @@ export default {
       this.connected = connected;
       this.$store.dispatch("Connect", connected);
       // console.log(this.connected)
-      if (connected == false) {
-        this.car = "red";
-      } else {
-        this.car = "blue";
+      if (connected == true) {
         this.Send("drive", this.clientId + " har anslutits.");
       }
-    },
+    }, // funktionen som faktiskt skickar alla meddelanden
     Send(message) {
       this.client.publish("joel.andersson@abbindustrigymnasium.se/drive", message);
+      // console.log så att jag kan se att funkar utan att behöva sättta på arduinon
      console.log(message);
     },
-  },
+  },// data
   data: () => ({
     ang: "090",
     direc: "f",
